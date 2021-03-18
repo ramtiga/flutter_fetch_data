2c6fe10440eb75c5c52708cd05207ea271d2ae36
dhane31 <dhanegm731@gmail.com>
2021-03-18 11:14:21 +0900

    Add refresh indicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fetch_data/state_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'model/user.dart';
 
 void main() {
@@ -28,51 +29,64 @@ class MyHomePage extends ConsumerWidget {
       T Function<T>(ProviderBase<Object, T> provider) watch) {
     AsyncValue<List<User>> users = watch(userStateFuture);
     return Scaffold(
-      appBar: AppBar(title: Text('Listing Users')),
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('Listing Users'),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
       body: users.when(
           loading: () => const Center(child: CircularProgressIndicator()),
           error: (error, stackTrace) =>
               Center(child: Text('${error.toString()}')),
           data: (value) {
-            return ListView.builder(
-                itemCount: value.length,
-                itemBuilder: (context, index) {
-                  var fullName = value[index].name.title +
-                      " " +
-                      value[index].name.first +
-                      " " +
-                      value[index].name.last;
-                  return Column(
-                    children: [
-                      ListTile(
-                        title: Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage(value[index].picture.large),
-                            ),
-                            SizedBox(width: 20.0),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+            return RefreshIndicator(
+              onRefresh: () async {
+                await context.refresh(userStateFuture);
+              },
+              child: ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    var fullName = value[index].name.title +
+                        " " +
+                        value[index].name.first +
+                        " " +
+                        value[index].name.last;
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Row(
                               children: [
-                                Text(
-                                  fullName.toString(),
-                                  style: TextStyle(fontSize: 17.0),
+                                CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage(value[index].picture.large),
+                                  radius: 24.0,
                                 ),
-                                SizedBox(height: 10.0),
-                                Text(
-                                  value[index].email.toString(),
-                                  style: TextStyle(
-                                      fontSize: 15.0, color: Colors.grey),
-                                ),
+                                SizedBox(width: 20.0),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      fullName.toString(),
+                                      style: TextStyle(fontSize: 17.0),
+                                    ),
+                                    SizedBox(height: 10.0),
+                                    Text(
+                                      value[index].email.toString(),
+                                      style: TextStyle(
+                                          fontSize: 15.0, color: Colors.grey),
+                                    ),
+                                  ],
+                                )
                               ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  );
-                });
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  }),
+            );
           }),
     );
   }
